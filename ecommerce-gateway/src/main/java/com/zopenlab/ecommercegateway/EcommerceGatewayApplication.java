525d4a02b4154e125fd2828edc66e29d5b79f916
kaskinas <kaskinas@yahoo.fr>
2020-04-13 22:33:27 +0000

    creation gateway

---
+++ ecommerce-gateway/src/main/java/com/zopenlab/ecommercegateway/EcommerceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zopenlab.ecommercegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceGatewayApplication.class, args);
+	}
+
+}
