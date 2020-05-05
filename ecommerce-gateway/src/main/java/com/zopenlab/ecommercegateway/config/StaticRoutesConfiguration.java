a5e8a6389d409f512eaba4f9685e644361feb472
kaskinas <kaskinas@yahoo.fr>
2020-05-05 20:50:28 +0000

    toil 4

--- ecommerce-gateway/src/main/java/com/zopenlab/ecommercegateway/config/StaticRoutesConfiguration.java
+++ ecommerce-gateway/src/main/java/com/zopenlab/ecommercegateway/config/StaticRoutesConfiguration.java
@@ -1,9 +1,7 @@
 package com.zopenlab.ecommercegateway.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
