e6a1bc6c14a4ced4d2ead07dd5577249cb2fadd4
kaskinas <kaskinas@yahoo.fr>
2020-05-06 12:29:10 +0000

    toil2 SecurityConfiguration gateway

--- ecommerce-gateway/src/main/java/com/zopenlab/ecommercegateway/config/SecurityConfiguration.java
+++ ecommerce-gateway/src/main/java/com/zopenlab/ecommercegateway/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.zopenlab.ecommercegateway.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
@@ -8,6 +9,7 @@ import org.springframework.security.oauth2.client.registration.ReactiveClientReg
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter.Mode;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
 
