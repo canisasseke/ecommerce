65cf4470f967062356615bd142e44146bc2ba26d
kaskinas <kaskinas@yahoo.fr>
2020-05-06 19:41:39 +0000

    add FeignClientInterceptor in orders

--- ecommerce-orders/src/main/java/com/zopenlab/ecommerceorders/proxies/CustormerServiceProxy.java
+++ ecommerce-orders/src/main/java/com/zopenlab/ecommerceorders/proxies/CustormerServiceProxy.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
 import com.zopenlab.ecommerceorders.beans.CustomerBean;
 
 
@@ -12,7 +13,7 @@ import com.zopenlab.ecommerceorders.beans.CustomerBean;
 @FeignClient(name = "ecommerce-customers")
 @RibbonClient(name = "ecommerce-customers")
 public interface CustormerServiceProxy {
-	
+
 	@GetMapping("/customers/{customerid}")
 	public CustomerBean getCustomerById(@PathVariable(name = "customerid") Long customerid);
 
