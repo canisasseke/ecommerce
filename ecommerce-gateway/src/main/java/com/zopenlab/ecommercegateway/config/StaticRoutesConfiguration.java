98960aacded7d614a0375601569da3396190855b
kaskinas <kaskinas@yahoo.fr>
2020-05-13 12:33:53 +0000

    commentaire de StaticRoutesConfiguration

--- ecommerce-gateway/src/main/java/com/zopenlab/ecommercegateway/config/StaticRoutesConfiguration.java
+++ ecommerce-gateway/src/main/java/com/zopenlab/ecommercegateway/config/StaticRoutesConfiguration.java
@@ -1,30 +1,28 @@
-
-  package com.zopenlab.ecommercegateway.config;
-  
-  import org.springframework.cloud.gateway.route.RouteLocator; import
-  org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder; import
-  org.springframework.context.annotation.Bean; import
-  org.springframework.context.annotation.Configuration;
-  
-  @Configuration 
-  public class StaticRoutesConfiguration {
-  
-  
-  @Bean 
-  RouteLocator staticRoutes(RouteLocatorBuilder builder) { return
-  builder.routes() .route("products_path",r -> r.path("/products/**")
-  .uri("lb://ecommerce-products"))
-  
-  
-  .route("customers-path",r -> r.path("/customers/**")
-  .uri("lb://ecommerce-customers"))
-  
-  
-  .route("orders_path",r -> r.path("/orders/**") .uri("lb://ecommerce-orders"))
-  
-  
-  .build(); }
-  
-  
-  }
- 
+/*
+ * package com.zopenlab.ecommercegateway.config;
+ * 
+ * import org.springframework.cloud.gateway.route.RouteLocator; import
+ * org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder; import
+ * org.springframework.context.annotation.Bean; import
+ * org.springframework.context.annotation.Configuration;
+ * 
+ * @Configuration public class StaticRoutesConfiguration {
+ * 
+ * 
+ * @Bean RouteLocator staticRoutes(RouteLocatorBuilder builder) { return
+ * builder.routes() .route("products_path",r -> r.path("/products/**")
+ * .uri("lb://ecommerce-products"))
+ * 
+ * 
+ * .route("customers-path",r -> r.path("/customers/**")
+ * .uri("lb://ecommerce-customers"))
+ * 
+ * 
+ * .route("orders_path",r -> r.path("/orders/**") .uri("lb://ecommerce-orders"))
+ * 
+ * 
+ * .build(); }
+ * 
+ * 
+ * }
+ */
